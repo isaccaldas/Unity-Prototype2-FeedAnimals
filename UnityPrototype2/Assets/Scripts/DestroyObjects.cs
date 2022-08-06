9aaa853cd6e8541ccf075038941898119d0e23e0
isaccaldas <34041465+isaccaldas@users.noreply.github.com>
2022-08-06 11:29:41 -0300

    aplicando o script de para os prefabs

--- UnityPrototype2/Assets/Scripts/DestroyObjects.cs
+++ UnityPrototype2/Assets/Scripts/DestroyObjects.cs
@@ -6,7 +6,7 @@ public class DestroyObjects : MonoBehaviour
 {
 
     private float topBound = 30;
-
+    private float lowerBound = -15;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +16,11 @@ public class DestroyObjects : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(transform.position.z > topBound)
-        Destroy(gameObject);
+        if(transform.position.z > topBound){
+            Destroy(gameObject);
+        }
+        else if(transform.position.z < topBound){
+            Destroy(gameObject);
+            }      
     }
 }
