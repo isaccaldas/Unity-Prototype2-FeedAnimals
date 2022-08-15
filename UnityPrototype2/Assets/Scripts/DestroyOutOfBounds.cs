7b40363452a128c33a2ab76d1dfaf334420ef5a8
isaccaldas <34041465+isaccaldas@users.noreply.github.com>
2022-08-15 09:43:27 -0300

    correção de  bugs

--- UnityPrototype2/Assets/Scripts/DestroyOutOfBounds.cs
+++ UnityPrototype2/Assets/Scripts/DestroyOutOfBounds.cs
@@ -2,11 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DestroyObjects : MonoBehaviour
+public class DestroyOutOfBounds : MonoBehaviour
 {
 
     private float topBound = 30;
     private float lowerBound = -15;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,11 +17,12 @@ public class DestroyObjects : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(transform.position.z > topBound){
+        if (transform.position.z > topBound) {
+            Destroy(gameObject); 
+        }
+
+        else if (transform.position.z < lowerBound){
             Destroy(gameObject);
         }
-        else if(transform.position.z < topBound){
-            Destroy(gameObject);
-            }      
     }
 }
