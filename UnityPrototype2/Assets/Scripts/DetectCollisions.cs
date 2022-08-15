3de57c0dc5f9ac397bf38db994ca9fc4cb9a0ad0
isaccaldas <34041465+isaccaldas@users.noreply.github.com>
2022-08-15 20:46:43 -0300

    Trigger a “Game Over” message

--- UnityPrototype2/Assets/Scripts/DetectCollisions.cs
+++ UnityPrototype2/Assets/Scripts/DetectCollisions.cs
@@ -18,6 +18,7 @@ public class DetectCollisions : MonoBehaviour
 
     private void OnTriggerEnter(Collider other) {
         
+      
         Destroy(gameObject); 
         Destroy(other.gameObject);
 
