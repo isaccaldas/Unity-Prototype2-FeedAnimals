8eec62263e2b1c5c53d5bd6ea1ebba5bb5bfdb00
isaccaldas <34041465+isaccaldas@users.noreply.github.com>
2022-08-15 10:25:39 -0300

    Spawn random animals from array

--- UnityPrototype2/Assets/Scripts/SpawnManager.cs
+++ UnityPrototype2/Assets/Scripts/SpawnManager.cs
@@ -6,7 +6,7 @@ public class SpawnManager : MonoBehaviour
 {
 
     public GameObject[ ] animalPrefabs;
-
+   
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +16,18 @@ public class SpawnManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.S)){
+            
+            // A variável animalIndex recebe um valor aleatório de zero ao tamanho máximo do array.
+            int animalIndex = Random.Range(0,animalPrefabs.Length);
+
+            //Cria um objeto toda vez que a tecla s é pressionada.
+            //new Vector3(0,0,20) é a posiçao inicial que o objeto será criado
+            //animalPrefabs[animalIndex].transform.rotation indica a rotaçao inicial desse objeto.
+            //animalPrefabs[animalIndex] é um array onde cada posiçao se trata de um objeto.
+            
+            Instantiate(animalPrefabs[animalIndex], new Vector3(0,0,20), 
+            animalPrefabs[animalIndex].transform.rotation);
+        }   
     }
 }
